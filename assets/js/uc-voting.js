d603542e41f6fe8f41ed46680509e0f5ef0bfe84
Cesar Marrujo <CMarrujo@esri.com>
2015-11-04 00:02:10 -0800

    Code clean up, removed code that is no longer needed.

--- assets/js/uc-voting.js
+++ assets/js/uc-voting.js
@@ -27,27 +27,6 @@ function handleSuccess(results) {
 		}
 }
 
-function handleError(xhr, status, error) {
-	$('#form-content').fadeOut();
-	$('#error-display').fadeIn();
-	$('#form-header #title h2').html("There was an error processing your vote");
-}
-
-function submitForm(payload) {
-	$.ajax({
-		url:apiURL,
-		method:"GET",
-		dataType:"jsonp",
-		data:{payload:JSON.stringify(payload)},
-		success:handleSuccess,
-		error:handleError
-	});
-}
-
-function getMaxOfArray(numArray) {
-    return Math.max.apply(null, numArray);
-}
-
 function chooseLogo() {
 	var user = 	{};
 	try{
@@ -73,12 +52,6 @@ function resetSelected() {
 	$('.choose').removeClass('add-white-txt');
 }
 
-function changeLogoSelection() {
-	$('#logo-selected').next('a').click(function() {
-		removeLightbox();
-	});
-}
-
 function userSelection(selection) {
 //		VotingAPI.castVote(selection).then(function(response) {
 //			showSuccess(selection, response);
@@ -94,32 +67,4 @@ function showSuccess(selection, response) {
 
 function showFail(response) {
 	$('<div class="voting-error">'+response+'</div>').appendTo('body');
-}
-
-function ucVotingSlider() {
-	$('.slide-btn').click(function() {
-		var dir = $(this).hasClass('prev-slide') ? "left" : "right";
-		var logo = $(this).siblings('.logo');	
-		advanceSlider(dir, logo);
-	});
-	$('.logo').click(function() {
-		if($(this).siblings('.slide-btn').css('display') === 'none'){
-			advanceSlider("right", this);
-		}
-	});
-}
-
-function advanceSlider(dir, logo) {
-		var curIndex = parseInt($(logo).data('logo'));
-		if(dir == "left") {
-			curIndex--;
-			curIndex = curIndex == 0 ? 4 : curIndex;
-		}else{
-			curIndex++;
-			curIndex = curIndex == 5 ? 1 : curIndex;
-		}
-		$(logo).removeClass(function(i, css) {
-			return (css.match(/(^|\s)img\S+/g) || []).join(" ");
-		});
-		$(logo).addClass('img' + curIndex).data('logo', curIndex);
 }
